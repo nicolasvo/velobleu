1cdc920a5ed215a082b5c1148c58b8c2c3a89db7
nico <nicolasvo95@gmail.com>
2022-04-25 22:34:43 +0200

    add available bikes and slots

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,24 +1,7 @@
 <script>
-	import * as JSON5 from 'json5';
-	import { stations } from './store.js';
 	import Map from './map.svelte';
-
-	fetch('https://www.velobleu.org/en/nos-stations/')
-		.then((response) => response.text())
-		.then((text) => {
-			const pattern = /var stations([^;]+)/g;
-			const match = text
-				.replace(/\n/g, '')
-				.match(pattern)[0]
-				.replace(/var stations = /g, '');
-			$stations = JSON5.parse(match);
-			console.log($stations);
-		});
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<!-- {#each $stations as station}
-    <p>{station["name"]}</p>
-{/each} -->
-<!-- <Map /> -->
+<Map />
--- src/routes/map.svelte
+++ src/routes/map.svelte
@@ -1,75 +1,44 @@
-<!-- <script>
-    // https://svelte.dev/tutorial/context-api
-	import * as L from 'leaflet';
-	import { stations } from './store.js';
-	let map;
-
-	function createMap(container) {
-		let m = L.map(container).setView([43.67, 7.21], 13);
-		L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-			attribution: `&copy;<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>,
-            &copy;<a href="https://carto.com/attributions" target="_blank">CARTO</a>`,
-			subdomains: 'abcd',
-			maxZoom: 20
-		}).addTo(m);
-
-		let marker = L.marker([43.67, 7.21]).addTo(m);
-		if ($stations.length > 0) {
-			$stations.forEach((station) => {
-				console.log(station.latlng);
-				L.marker(station.latlng).addTo(m);
-			});
-		}
-
-		return m;
-	}
-
-	function mapAction(container) {
-		map = createMap(container);
-		return {
-			destroy: () => {
-				map.remove();
-			}
-		};
-	}
-</script>
-
-<svelte:head>
-	<link
-		rel="stylesheet"
-		href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
-		integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
-		crossorigin=""
-	/>
-</svelte:head>
-
-<div style="height:90vh;width:100%" use:mapAction /> -->
 <script>
 	import { onMount } from 'svelte';
-	import { stations } from './store.js';
 	import * as L from 'leaflet';
+	import json5 from 'json5';
+
+	const getStations = async () => {
+		const match = await fetch(
+			`https://api.allorigins.win/get?url=${encodeURIComponent(
+				'https://www.velobleu.org/en/nos-stations/'
+			)}`
+		)
+			.then((response) => response.text())
+			.then((text) => {
+				const pattern = /var stations([^;]+)/g;
+				const match = text
+					.replace(/\\n/g, '')
+					.replace(/\\/g, '')
+					.match(pattern)[0]
+					.replace(/var stations = /g, '');
+				return match;
+			});
+		return match;
+	};
 
 	let map;
 
-	$: if ($stations.length == 0) {
-		console.log(`no stations`);
-		onMount(async () => {
-			map = L.map('map').setView([43.67, 7.21], 13);
-			L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png ', {
-				attribution:
-					'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-				maxZoom: 20
-			}).addTo(map);
+	onMount(async () => {
+		map = L.map('map').setView([43.67, 7.21], 13);
+		L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png ', {
+			attribution:
+				'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+			maxZoom: 18
+		}).addTo(map);
 
-			L.marker([43.67, 7.21]).addTo(map);
+		const stations = await getStations().then((match) => json5.parse(match));
+
+		stations.forEach((station) => {
+			const marker = L.marker(station['latlng']).addTo(map);
+			marker.bindTooltip(`Free bikes: ${station["ab"]}<br>Free slots: ${station["ap"]}`);
 		});
-	} else {
-		console.log('hm?');
-	}
-
-	$: {
-		console.log($stations);
-	}
+	});
 </script>
 
 <svelte:head>
@@ -85,6 +54,8 @@
 		integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
 		crossorigin="">
 	</script>
+
+	<script src="https://unpkg.com/json5@^2.0.0/dist/index.min.js"></script>
 </svelte:head>
 
 <div id="map" />
